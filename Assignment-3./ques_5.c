008f0a12789ca0605bb6709c60d73aa0454c091f
harishteena9813-sketch <harishteena9813@gmail.com>
2026-09-06 21:15:17 +0530

    Add Two Complex Numbers by Passing Structure to a Function

---
+++ Assignment-3./ques_5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+struct Complex
+{
+    float real;
+    float imag;
+};
+
+struct Complex addComplex(struct Complex c1, struct Complex c2)
+{
+    struct Complex result;
+
+    result.real = c1.real + c2.real;
+    result.imag = c1.imag + c2.imag;
+
+    return result;
+}
+
+int main()
+{
+    struct Complex c1, c2, result;
+
+    printf("Enter real and imaginary parts of first complex number: ");
+    scanf("%f %f", &c1.real, &c1.imag);
+
+    printf("Enter real and imaginary parts of second complex number: ");
+    scanf("%f %f", &c2.real, &c2.imag);
+
+    result = addComplex(c1, c2);
+
+    printf("\nFirst complex number = %.2f + %.2fi\n",
+           c1.real, c1.imag);
+
+    printf("Second complex number = %.2f + %.2fi\n",
+           c2.real, c2.imag);
+
+    printf("Sum = %.2f + %.2fi\n",
+           result.real, result.imag);
+
+    return 0;
+}
